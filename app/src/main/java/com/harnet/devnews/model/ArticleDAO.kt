475648aed3bdad721e78d81c751c74481f8a4bf7
Harnet69 <harnet69@gmail.com>
2020-09-30 20:14:26 +0200

    Implemented ArticleDAO interface with CRUD

--- app/src/main/java/com/harnet/devnews/model/ArticleDAO.kt
+++ app/src/main/java/com/harnet/devnews/model/ArticleDAO.kt
@@ -1,6 +1,16 @@
 package com.harnet.devnews.model
 
+import androidx.room.Insert
+import androidx.room.Query
+
 
 interface ArticleDAO {
+    @Insert
+    suspend fun insertAll(vararg article: Article): List<Long>
 
+    @Query("SELECT * FROM article")
+    suspend fun getArticles(): List<Article>
+
+    @Query("SELECT * FROM article WHERE uuid = :articleId")
+    suspend fun getArticle(articleId: Int): Article
 }
---
+++ app/src/main/java/com/harnet/devnews/model/ArticleDatabase.kt
@@ -0,0 +1,4 @@
+package com.harnet.devnews.model
+
+class ArticleDatabase {
+}
