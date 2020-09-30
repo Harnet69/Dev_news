cf5576ee8ed346bd0845c031813572e55d2c725c
Harnet69 <harnet69@gmail.com>
2020-09-30 20:22:12 +0200

    Implemented singleton as ArticleDatabase Class

--- app/src/main/java/com/harnet/devnews/model/ArticleDatabase.kt
+++ app/src/main/java/com/harnet/devnews/model/ArticleDatabase.kt
@@ -1,4 +1,29 @@
 package com.harnet.devnews.model
 
-class ArticleDatabase {
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class ArticleDatabase: RoomDatabase() {
+    abstract fun articleDAO(): ArticleDAO
+
+    companion object{
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            // create an instance of DogDatabase
+            instance ?: buildDatabase(context).also {
+                // attach an instance to variable and return an instance to invoker
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            ArticleDatabase::class.java,
+            "articlesDatabase"
+        ).build()
+    }
 }
