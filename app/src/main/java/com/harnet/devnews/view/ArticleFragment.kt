3828cdd8ffaf927c4631910d8f369dd85b4dbd2f
Harnet69 <harnet69@gmail.com>
2020-10-03 19:27:10 +0200

    URL is underlined now

--- app/src/main/java/com/harnet/devnews/view/ArticleFragment.kt
+++ app/src/main/java/com/harnet/devnews/view/ArticleFragment.kt
@@ -1,19 +1,22 @@
 package com.harnet.devnews.view
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.harnet.devnews.R
-import com.harnet.devnews.model.ArticleDatabase
 import com.harnet.devnews.viewModel.ArticleViewModel
-import com.harnet.devnews.viewModel.ArticlesListViewModel
 import kotlinx.android.synthetic.main.fragment_article.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+
 
 class ArticleFragment : Fragment() {
     lateinit var viewModel: ArticleViewModel
@@ -41,22 +44,30 @@ class ArticleFragment : Fragment() {
                 viewModel.fetch(view.context, articleId.toString())
             }
         }
-
+        openWebsite(article_url)
         observeViewModel()
     }
 
     // observes article object and binds its data to view elements
     fun observeViewModel(){
         viewModel.mArticleLiveData.observe(this, Observer { article ->
-            article?.let{
+            article?.let {
                 article_id.text = article.id
                 article_title.text = article.title
                 article_author.text = article.author
                 article_time.text = article.time
                 article_score.text = article.score
+                article_url.paintFlags = article_url.paintFlags or Paint.UNDERLINE_TEXT_FLAG
                 article_url.text = article.url
             }
         })
     }
 
+    private fun openWebsite(view: TextView?) {
+        view?.setOnClickListener {
+            val webPage = view.text.toString()
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(webPage))
+            startActivity(browserIntent)
+        }
+    }
 }
--- app/src/main/java/com/harnet/devnews/viewModel/ArticleViewModel.kt
+++ app/src/main/java/com/harnet/devnews/viewModel/ArticleViewModel.kt
@@ -15,18 +15,17 @@ class ArticleViewModel(application: Application) : BaseViewModel(application) {
     //retrieve data from a database by id
     fun fetch(context: Context, articleId: String){
         launch {
-            val article: Article =
-                ArticleDatabase.invoke(context).articleDAO().getArticle(articleId.toInt())
+            val article: Article = ArticleDatabase.invoke(context).articleDAO().getArticle(articleId.toInt())
             //mock data for testing
-            Log.i("articleIdToDisplay", "fetch: $article")
-            val article1 = Article(
-                articleId,
-                "This electrical transmission tower",
-                "Twitter",
-                "www.somewhere.com",
-                "54",
-                "4"
-            )
+//            Log.i("articleIdToDisplay", "fetch: $article")
+//            val article1 = Article(
+//                articleId,
+//                "This electrical transmission tower",
+//                "Twitter",
+//                "www.somewhere.com",
+//                "54",
+//                "4"
+//            )
             mArticleLiveData.value = article
         }
     }
