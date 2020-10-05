658dc672717c2e8f41056ca6942e4ab8e9da266f
Harnet69 <harnet69@gmail.com>
2020-10-05 09:55:25 +0200

    Implement a pageContent parsing

--- app/src/main/java/com/harnet/devnews/viewModel/ArticleViewModel.kt
+++ app/src/main/java/com/harnet/devnews/viewModel/ArticleViewModel.kt
@@ -6,19 +6,23 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.harnet.devnews.model.Article
 import com.harnet.devnews.model.ArticleDatabase
+import com.harnet.devnews.service.WebContentDownloader
 import kotlinx.coroutines.launch
 import java.lang.Exception
-import java.text.SimpleDateFormat
 import java.util.*
 
 class ArticleViewModel(application: Application) : BaseViewModel(application) {
     val mArticleLiveData = MutableLiveData<Article>()
+    val webContentDownloader = WebContentDownloader()
 
     //retrieve data from a database by id
     fun fetch(context: Context, articleId: String) {
         launch {
             var articleToShow = ArticleDatabase.invoke(context).articleDAO().getArticle(articleId.toInt())
+            Log.i("ArticleData", "fetch: " + articleToShow.url)
             try {
+                val pageContent = webContentDownloader.execute(articleToShow.url).get()
+                Log.i("ArticleData", "fetch: " + pageContent)
                 val articleDate = Date(articleToShow.time.toLong() * 1000)
                 articleToShow.time = articleDate.toString()
                 Log.i("ArticleData", "fetch: " + articleDate)
