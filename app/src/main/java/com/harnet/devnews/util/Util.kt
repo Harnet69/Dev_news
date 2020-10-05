ef8a52b60541a26bf9aa3be81990dd4a057d2f11
Harnet69 <harnet69@gmail.com>
2020-10-05 18:42:00 +0200

    Glide listener works!

--- app/src/main/java/com/harnet/devnews/util/Util.kt
+++ app/src/main/java/com/harnet/devnews/util/Util.kt
@@ -1,10 +1,16 @@
 package com.harnet.devnews.util
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.harnet.devnews.R
 
 //little loading spinner for image loading
@@ -24,5 +30,27 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
     Glide.with(context)
         .setDefaultRequestOptions(options)
         .load(uri)
+        .addListener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.i("ListenGlide", "onLoadFailed: FAIL")
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+
+        })
         .into(this)// this - extended ImageView class
 }
