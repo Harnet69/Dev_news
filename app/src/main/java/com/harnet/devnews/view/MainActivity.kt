39760673d89e9a14990a5822b95798c7760d93bf
Harnet69 <harnet69@gmail.com>
2020-10-07 10:22:39 +0200

    Option menu was created

--- app/src/main/java/com/harnet/devnews/view/MainActivity.kt
+++ app/src/main/java/com/harnet/devnews/view/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.harnet.devnews.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.harnet.devnews.R
 
 class MainActivity : AppCompatActivity() {
+    // create application menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // interacting with menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        var isSelected = false
+        when (item.itemId) {
+            R.id.favourites -> {
+                isSelected = true
+                //TODO here start a new fragment from menu
+                Log.i("FavouritesArt", "onOptionsItemSelected: FAVOURITES")
+                Toast.makeText(this, "Favourites", Toast.LENGTH_SHORT).show()
+//                startFragment(ProfileFragment(), Fragments.PROFILE.toString())
+            }
+            R.id.newsRadar -> {
+                isSelected = true
+                //TODO here start a new fragment from menu
+                Log.i("FavouritesArt", "onOptionsItemSelected: NEWS RADAR")
+                Toast.makeText(this, "News radar", Toast.LENGTH_SHORT).show()
+//                startFragment(ProfileFragment(), Fragments.PROFILE.toString())
+            }
+        }
+        return isSelected
+    }
+
     // back arrow
     lateinit var navController: NavController
 
