c5ec4895638472ad0d64d5864829b65ba17c9157
Harnet69 <harnet69@gmail.com>
2020-10-14 14:21:00 +0200

    Implemented DataBinding boilerplate

--- app/src/main/java/com/harnet/devnewsradar/adapter/ArticlesListAdapter.kt
+++ app/src/main/java/com/harnet/devnewsradar/adapter/ArticlesListAdapter.kt
@@ -3,9 +3,11 @@ package com.harnet.devnewsradar.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.harnet.devnewsradar.R
+import com.harnet.devnewsradar.databinding.ItemAtricleBinding
 import com.harnet.devnewsradar.model.Article
 import com.harnet.devnewsradar.view.ArticlesListFragmentDirections
 import kotlinx.android.synthetic.main.item_atricle.view.*
@@ -24,7 +26,10 @@ class ArticlesListAdapter(val articlesList: ArrayList<Article>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         val inflator = LayoutInflater.from(parent.context)
         // elements of the list transforms into views
-        val view = inflator.inflate(R.layout.item_atricle, parent, false)
+        //classic approach
+//        val view = inflator.inflate(R.layout.item_atricle, parent, false)
+        // DataBinding approach
+        val view = DataBindingUtil.inflate<ItemAtricleBinding>(inflator,R.layout.item_atricle, parent, false)
         return ArticleViewHolder(view)
     }
 
@@ -32,21 +37,25 @@ class ArticlesListAdapter(val articlesList: ArrayList<Article>) :
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val isFavourite = false
-        //attach view to information from a list
-        holder.view.articleTitle_in_list.text = articlesList[position].title
-        holder.view.articleAuthor_in_list.text = articlesList[position].author
 
-        //add click listener to article details item and bind it with detail page
-        holder.view.article_details.setOnClickListener {
-            // navigate to appropriate detail fragment
-            val action =
-                ArticlesListFragmentDirections.actionArticlesListFragmentToArticleFragment()
-            // send article id to ArticleFragment
-            action.articleId = articlesList[position].uuid
-            action.isFavourite = isFavourite
-            Navigation.findNavController(it).navigate(action)
-        }
+        //attach article to holder by DataBinding approach to variable in the layout
+        holder.view.article = articlesList[position]
+
+        //attach view to information from a list by classic way
+//        holder.view.articleTitle_in_list.text = articlesList[position].title
+//        holder.view.articleAuthor_in_list.text = articlesList[position].author
+//
+//        //add click listener to article details item and bind it with detail page
+//        holder.view.article_details.setOnClickListener {
+//            // navigate to appropriate detail fragment
+//            val action =
+//                ArticlesListFragmentDirections.actionArticlesListFragmentToArticleFragment()
+//            // send article id to ArticleFragment
+//            action.articleId = articlesList[position].uuid
+//            action.isFavourite = isFavourite
+//            Navigation.findNavController(it).navigate(action)
+//        }
     }
 
-    class ArticleViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class ArticleViewHolder(var view: ItemAtricleBinding) : RecyclerView.ViewHolder(view.root)
 }
