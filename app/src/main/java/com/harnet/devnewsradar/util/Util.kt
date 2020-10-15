936fe5768e5bc8ec92e10d88d0a6ed13ac260572
Harnet69 <harnet69@gmail.com>
2020-10-15 10:33:39 +0200

    Add click listener to article list items

--- app/src/main/java/com/harnet/devnewsradar/util/Util.kt
+++ app/src/main/java/com/harnet/devnewsradar/util/Util.kt
@@ -3,7 +3,10 @@ package com.harnet.devnewsradar.util
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.navigation.Navigation
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -12,6 +15,7 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import com.harnet.devnewsradar.R
+import com.harnet.devnewsradar.view.ArticlesListFragmentDirections
 
 //little loading spinner for image loading
 fun getProgressDrawable(context: Context): CircularProgressDrawable {
@@ -52,4 +56,27 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
 
         })
         .into(this)// this - extended ImageView class
+}
+
+// load image by ViewBinding(uses extended load function to bind this function to imageView)
+// @BindingAdapter annotation make function visible from a layout (parameter fill be the name of xml field)
+// !!!REBUILD PROJECT AFTER IMPLEMENTING!!!
+@BindingAdapter("android:bindImageUrl")
+fun loadBindingImage(view: ImageView, url: String?){
+    view.loadImage(url, getProgressDrawable(view.context))
+}
+
+// transition to an article detail page
+@BindingAdapter("android:goToArticle")
+fun goToArticle(view: View, articleUuid: Int?){
+    view.setOnClickListener {
+            // navigate to appropriate detail fragment
+            val action =
+                ArticlesListFragmentDirections.actionArticlesListFragmentToArticleFragment()
+            // send article id to ArticleFragment
+        if (articleUuid != null) {
+            action.articleId = articleUuid
+        }
+            Navigation.findNavController(it).navigate(action)
+        }
 }
