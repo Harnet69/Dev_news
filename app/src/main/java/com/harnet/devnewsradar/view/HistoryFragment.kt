450ffc2fb76164c1a882497905f34f3dfd45b0ed
Harnet69 <harnet69@gmail.com>
2020-10-26 12:19:50 +0100

    History ModelView was partly implemented

--- app/src/main/java/com/harnet/devnewsradar/view/HistoryFragment.kt
+++ app/src/main/java/com/harnet/devnewsradar/view/HistoryFragment.kt
@@ -6,16 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.harnet.devnewsradar.viewModel.HistoryViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.harnet.devnewsradar.R
+import com.harnet.devnewsradar.adapter.ArticlesListAdapter
+import com.harnet.devnewsradar.viewModel.ArticlesListViewModel
+import kotlinx.android.synthetic.main.history_fragment.*
 
 class HistoryFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = HistoryFragment()
-    }
-
-    private lateinit var viewModel: HistoryViewModel
+    private lateinit var viewModel: ArticlesListViewModel
+    lateinit var historyListAdapter: ArticlesListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +23,19 @@ class HistoryFragment : Fragment() {
         return inflater.inflate(R.layout.history_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        historyListAdapter = ArticlesListAdapter(arrayListOf())
+        viewModel = ViewModelProvider(this).get(ArticlesListViewModel::class.java)
+        viewModel.refresh()
+
+            history_list.apply {
+                layoutManager = LinearLayoutManager(context)
+                //Fix blinking RecyclerView
+                historyListAdapter.setHasStableIds(true)
+                adapter = historyListAdapter
+            }
     }
 
 }
