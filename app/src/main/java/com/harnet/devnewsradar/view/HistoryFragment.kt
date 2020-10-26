f294f25627d75bb38e3dce70c13c8d75b1669e3a
Harnet69 <harnet69@gmail.com>
2020-10-26 12:37:58 +0100

    History RecyclerView shows items

--- app/src/main/java/com/harnet/devnewsradar/view/HistoryFragment.kt
+++ app/src/main/java/com/harnet/devnewsradar/view/HistoryFragment.kt
@@ -6,14 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.harnet.devnewsradar.R
 import com.harnet.devnewsradar.adapter.ArticlesListAdapter
 import com.harnet.devnewsradar.viewModel.ArticlesListViewModel
+import com.harnet.devnewsradar.viewModel.HistoryViewModel
+import kotlinx.android.synthetic.main.fragment_articles_list.*
 import kotlinx.android.synthetic.main.history_fragment.*
 
 class HistoryFragment : Fragment() {
-    private lateinit var viewModel: ArticlesListViewModel
+    private lateinit var viewModel: HistoryViewModel
     lateinit var historyListAdapter: ArticlesListAdapter
 
     override fun onCreateView(
@@ -27,15 +30,47 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         historyListAdapter = ArticlesListAdapter(arrayListOf())
-        viewModel = ViewModelProvider(this).get(ArticlesListViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
         viewModel.refresh()
 
-            history_list.apply {
-                layoutManager = LinearLayoutManager(context)
-                //Fix blinking RecyclerView
-                historyListAdapter.setHasStableIds(true)
-                adapter = historyListAdapter
-            }
+        history_list.apply {
+            layoutManager = LinearLayoutManager(context)
+            //Fix blinking RecyclerView
+            historyListAdapter.setHasStableIds(true)
+            adapter = historyListAdapter
+        }
+        observeViewModel()
     }
 
+    private fun observeViewModel() {
+        // update the layout using values of mutable variables from a ViewModel
+        viewModel.mArticles.observe(viewLifecycleOwner, Observer { articles ->
+            articles?.let {
+                history_list.visibility = View.VISIBLE
+                historyListAdapter.updateArticlesList(articles)
+            }
+        })
+
+        // make error TextViewVisible
+        viewModel.mIsArticleLoadError.observe(viewLifecycleOwner, Observer { isError ->
+            // check isError not null
+            isError?.let {
+                listErrorHistory.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        // loading spinner
+        viewModel.mIsLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            //check isLoading not null
+            isLoading?.let {
+                // if data still loading - show spinner, else - remove it
+                loadingProgressBarHistory.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    //hide all views when progress bar is visible
+                    listErrorHistory.visibility = View.GONE
+                    history_list.visibility = View.GONE
+                }
+            }
+        })
+    }
 }
--- app/src/main/java/com/harnet/devnewsradar/viewModel/HistoryViewModel.kt
+++ app/src/main/java/com/harnet/devnewsradar/viewModel/HistoryViewModel.kt
@@ -1,7 +1,33 @@
 package com.harnet.devnewsradar.viewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.harnet.devnewsradar.model.Article
+import com.harnet.devnewsradar.model.ArticleDatabase
+import kotlinx.coroutines.launch
 
-class HistoryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HistoryViewModel(application: Application): BaseViewModel(application){
+    val mArticles = MutableLiveData<List<Article>>()
+    val mIsArticleLoadError = MutableLiveData<Boolean>()
+    val mIsLoading = MutableLiveData<Boolean>()
+
+    // refresh mArticles with a new data
+    fun refresh() {
+        launch {
+            val historyList = ArticleDatabase.invoke(getApplication()).articleReadDAO().getArticles()
+
+            retrieveArticle(historyList)
+        }
+    }
+
+    // retrieve articles
+    private fun retrieveArticle(historyArticles: List<Article>) {
+        // set received list to observable mutable list
+        mArticles.postValue(historyArticles)
+        // switch off error message
+        mIsArticleLoadError.postValue(false)
+        // switch off waiting spinner
+        mIsLoading.postValue(false)
+    }
+
 }
