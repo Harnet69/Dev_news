bcb908eb13bee4b273b0749dd506027f90c281eb
Harnet69 <harnet69@gmail.com>
2020-10-28 14:01:13 +0100

    Change background color of settings page

--- app/src/main/java/com/harnet/devnewsradar/view/SettingsFragment.kt
+++ app/src/main/java/com/harnet/devnewsradar/view/SettingsFragment.kt
@@ -1,11 +1,22 @@
 package com.harnet.devnewsradar.view
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.harnet.devnewsradar.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // change page background
+        listView.setBackgroundResource(R.drawable.bgnd)
     }
 }
