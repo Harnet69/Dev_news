1f2698333414c167293a2824b1ee239d0e5446a1
Harnet69 <harnet69@gmail.com>
2020-11-11 18:14:27 +0100

    Sms sending was implemented

--- app/src/main/java/com/harnet/devnewsradar/model/Favourite.kt
+++ app/src/main/java/com/harnet/devnewsradar/model/Favourite.kt
@@ -1,8 +1,6 @@
 package com.harnet.devnewsradar.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 @Entity
 class Favourite(
--- app/src/main/java/com/harnet/devnewsradar/service/SMSable.kt
+++ app/src/main/java/com/harnet/devnewsradar/service/SMSable.kt
@@ -1,18 +1,27 @@
 package com.harnet.devnewsradar.service
 
+import android.app.PendingIntent
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import android.telephony.SmsManager
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.harnet.devnewsradar.R
 import com.harnet.devnewsradar.databinding.SendSmsDialogBinding
 import com.harnet.devnewsradar.model.Article
 import com.harnet.devnewsradar.model.SmsInfo
+import com.harnet.devnewsradar.view.MainActivity
 
 interface SMSable {
 
-    fun createSmsDialog(context: Context?, currentArticle: Article?, isSendSmsStarted: Boolean, permissionGranted: Boolean) {
+    fun createSmsDialog(
+        context: Context?,
+        currentArticle: Article?,
+        isSendSmsStarted: Boolean,
+        permissionGranted: Boolean
+    ) {
         // if SMS permission was granted
         if (isSendSmsStarted && permissionGranted) {
             context?.let {
@@ -41,15 +50,13 @@ interface SMSable {
                 AlertDialog.Builder(it)
                     .setView(dialogBinding.root)
                     .setPositiveButton("Send SMS") { dialog, which ->
-                        Log.i("SendSms", "createSmsDialog: pushed Send btn ")
                         // check is user put smth to 'recipient' field
                         if (!dialogBinding.smsRecipient.text.isNullOrEmpty()) {
                             if (smsInfo != null) {
                                 // get the text of form field 'to' and set it to entity
                                 smsInfo.to = dialogBinding.smsRecipient.text.toString()
-                                Log.i("SendSms", "createSmsDialog: SEND ")
                                 // send SMS
-                                sendSms(smsInfo)
+                                sendSms(context, smsInfo)
                             }
                         }
                     }
@@ -59,8 +66,14 @@ interface SMSable {
         }
     }
 
-    // when user sent SMS clicked to 'send SMS' button
-    private fun sendSms(smsInfo: SmsInfo) {
-        Log.i("SendSms", "sendSms: $smsInfo")
+    // when user sent SMS by clicking to 'send SMS' button
+    private fun sendSms(context: Context, smsInfo: SmsInfo) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(context, 0, intent, 0)
+        val sms = SmsManager.getDefault()
+        val smsMessage = "${smsInfo.text}: ${smsInfo.articleUrl}"
+
+        sms.sendTextMessage(smsInfo.to, null, smsMessage, pi, null)
+        Toast.makeText(context, "Article was sent to ${smsInfo.to}", Toast.LENGTH_LONG).show()
     }
 }
